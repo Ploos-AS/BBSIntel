046909138f1bffa44feca62efebe50471cea52ac
pos <pgousdal@users.noreply.github.com>
2026-09-09 02:56:52 +0200

    M0: define source adapter interface

---
+++ internal/source/source.go
@@ -0,0 +1,21 @@
+package source
+
+import "context"
+
+type Entry struct {
+	Source string
+	SourceKey string
+	SourceURL string
+	Name string
+	Software string
+	Country string
+	Description string
+	Protocol string
+	Hostname string
+	Port int
+}
+
+type Adapter interface {
+	Name() string
+	Fetch(context.Context) ([]Entry, error)
+}
