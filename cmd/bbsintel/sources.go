662784df6d1f47e5099ad446727728c35bb52e36
pos <pgousdal@users.noreply.github.com>
2026-09-09 03:50:33 +0200

    M1.1: register intelligence route

--- cmd/bbsintel/sources.go
+++ cmd/bbsintel/sources.go
@@ -19,6 +19,7 @@ type sourceView struct {
 
 func registerSourceRoutes(mux *http.ServeMux, srv *server) {
 	mux.HandleFunc("GET /api/v1/bbs/{id}/sources", srv.getBBSSources)
+	registerIntelRoutes(mux, srv)
 }
 
 func (s *server) getBBSSources(w http.ResponseWriter, r *http.Request) {
